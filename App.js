b112b866598acca7fe57e532ece1ae296286f639
R-Grigala <r.grigalashvili777@gmail.com>
2023-02-27 19:18:29 +0400

    Coding Continue

--- App.js
+++ App.js
@@ -3,11 +3,10 @@ import Home from "./screens/Home";
 import About from "./screens/About";
 import ReviewDetails from "./screens/ReviewDetails";
 import MapScreen from "./screens/MapScreen";
-import AppNavigator from "./navigation/Navigator";
 
 
 export default function App() {
   return (
-    <AppNavigator />
+    <MapScreen />
   );
 }
