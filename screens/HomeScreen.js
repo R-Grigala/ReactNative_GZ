27b7f72506f84ed2997d6baecc93d8d5ee50521f
R-Grigala <r.grigalashvili777@gmail.com>
2023-02-28 17:20:18 +0400

    Coding

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import { View, Text, StyleSheet, Button} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import EventList from '../components/events/EventList'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
   return (
     <View style={styles.screen}>
-      <Text>This is the Home Screen</Text>
-      <Button title="move to detail" onPress={()=> navigation.navigate('Event')}/>
+      <Text>Hello World</Text>
+      {/* <Button title="move to detail" onPress={()=> navigation.navigate('Event')}/> */}
+      <EventList />
     </View>
   )
 }
